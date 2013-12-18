80f81e50a67e240d394293617d208dece93b3e1d
diego10j.89 <diego10j.89@gmail.com>
2013-12-18 15:44:53 +0000

    CAMBIOS EN la pantalla

--- src/java/paq_transportes/pre_concepto.java
+++ src/java/paq_transportes/pre_concepto.java
@@ -16,36 +16,48 @@ import framework.componentes.Tabla;
 public class pre_concepto extends Pantalla {
 
     private Tabla tab_tabla = new Tabla();
+    private Tabla tab_tabla2 = new Tabla();
 
     public pre_concepto() {
         tab_tabla.setId("tab_tabla");
-        tab_tabla.setTabla("trans_concepto", "ide_concepto",1);
+        tab_tabla.setTabla("trans_concepto", "ide_concepto", 1);
         tab_tabla.setHeader("CONCEPTOS");
+        tab_tabla.agregarRelacion(tab_tabla2);
         tab_tabla.dibujar();
         PanelTabla pat_panel = new PanelTabla();
         pat_panel.setPanelTabla(tab_tabla);
 
+        tab_tabla2.setId("tab_tabla2");
+        tab_tabla2.setTabla("trans_detalle", "ide_detalle", 2);
+        tab_tabla2.setHeader("DETALLE CONCEPTOS");
+        tab_tabla2.getColumna("ide_tramite").setCombo("trans_tramites", "ide_tramite", "num_hoja_control,nombre", "");
+        tab_tabla2.dibujar();
+        PanelTabla pat_panel2 = new PanelTabla();
+        pat_panel2.setPanelTabla(tab_tabla2);
+
         Division div_division = new Division();
         div_division.setId("div_division");
-        div_division.dividir1(pat_panel);
+        div_division.dividir2(pat_panel, pat_panel2, "50%", "H");
         agregarComponente(div_division);
     }
 
     @Override
     public void insertar() {
-        tab_tabla.insertar();
+        utilitario.getTablaisFocus().insertar();
     }
 
     @Override
     public void guardar() {
         if (tab_tabla.guardar()) {
-            guardarPantalla();
+            if (tab_tabla2.guardar()) {
+                guardarPantalla();
+            }
         }
     }
 
     @Override
     public void eliminar() {
-        tab_tabla.eliminar();
+        utilitario.getTablaisFocus().eliminar();
     }
 
     public Tabla getTab_tabla() {
@@ -54,5 +66,13 @@ public class pre_concepto extends Pantalla {
 
     public void setTab_tabla(Tabla tab_tabla) {
         this.tab_tabla = tab_tabla;
+    }
+
+    public Tabla getTab_tabla2() {
+        return tab_tabla2;
+    }
+
+    public void setTab_tabla2(Tabla tab_tabla2) {
+        this.tab_tabla2 = tab_tabla2;
     }
 }
