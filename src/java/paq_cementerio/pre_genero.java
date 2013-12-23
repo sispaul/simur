d71c80c2b3a94199498e28e1d1b997c966824b5f
egallardo1730@gmail.com <egallardo1730@gmail.com@79e0af45-16aa-8f21-0652-82745544d00a>
2013-12-23 19:58:14 +0000

    CREACION DE PANTALLA

---
+++ src/java/paq_cementerio/pre_genero.java
@@ -0,0 +1,58 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paq_cementerio;
+
+import paq_sistema.*;
+import paq_sistema.aplicacion.Pantalla;
+import framework.componentes.Division;
+import framework.componentes.PanelTabla;
+import framework.componentes.Tabla;
+
+/**
+ *
+ * @author Diego
+ */
+public class pre_genero extends Pantalla {
+
+    private Tabla tab_tabla = new Tabla();
+
+    public pre_genero() {
+        tab_tabla.setId("tab_tabla");
+        tab_tabla.setTabla("CMT_GENERO", "IDE_CMGEN", 1);
+        tab_tabla.dibujar();
+        PanelTabla pat_panel = new PanelTabla();
+        pat_panel.setPanelTabla(tab_tabla);
+
+        Division div_division = new Division();
+        div_division.setId("div_division");
+        div_division.dividir1(pat_panel);
+        agregarComponente(div_division);
+    }
+
+    @Override
+    public void insertar() {
+        tab_tabla.insertar();
+    }
+
+    @Override
+    public void guardar() {
+        if (tab_tabla.guardar()) {
+            guardarPantalla();
+        }
+    }
+
+    @Override
+    public void eliminar() {
+        tab_tabla.eliminar();
+    }
+
+    public Tabla getTab_tabla() {
+        return tab_tabla;
+    }
+
+    public void setTab_tabla(Tabla tab_tabla) {
+        this.tab_tabla = tab_tabla;
+    }
+}
