e2a3fbce63262d7ab9339e5df541f3d8644f15ab
bena.nadove@gmail.com <bena.nadove@gmail.com@79e0af45-16aa-8f21-0652-82745544d00a>
2014-01-30 13:12:52 +0000

    cambio tabla medida

--- src/java/paq_bodega/pre_medida.java
+++ src/java/paq_bodega/pre_medida.java
@@ -24,7 +24,7 @@ public class pre_medida extends Pantalla {
         
         tab_tabla.setId("tab_tabla");
         tab_tabla.setConexion(con_postgres);
-        tab_tabla.setTabla("bodc_medida", "ide_medida", 1);
+        tab_tabla.setTabla("valc_medida", "ide_medida", 1);
         tab_tabla.setHeader("MEDIDA");
         tab_tabla.dibujar();
         
