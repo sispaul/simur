5007853d30bb90d4aac4790e1630eb65f32e78a7
santy22.atl <santy22.atl@gmail.com>
2014-03-12 15:11:06 +0000

    Coreccion Requerimientos

---
+++ src/java/paq_matriculas/pre_tipo_gestor.java
@@ -0,0 +1,58 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paq_matriculas;
+
+import framework.componentes.Division;
+import framework.componentes.PanelTabla;
+import framework.componentes.Tabla;
+import paq_sistema.aplicacion.Pantalla;
+
+/**
+ *
+ * @author p-sistemas
+ */
+public class pre_tipo_gestor extends Pantalla{
+
+    private Tabla tab_tabla = new Tabla();
+    
+    public pre_tipo_gestor() {
+        //id a la tabla
+        tab_tabla.setId("tab_tabla");
+        tab_tabla.setTabla("trans_tipo_gestor", "ide_tipo_gestor",1);
+        tab_tabla.setHeader("TIPOS DE GESTOR");
+        tab_tabla.dibujar();
+        PanelTabla pat_panel = new PanelTabla();
+        pat_panel.setPanelTabla(tab_tabla);
+
+        Division div_division = new Division();
+        div_division.setId("div_division");
+        div_division.dividir1(pat_panel);
+        agregarComponente(div_division);
+    }
+
+    @Override
+    public void insertar() {
+        tab_tabla.insertar();
+    }
+
+    @Override
+    public void guardar() {
+        tab_tabla.guardar();
+    }
+
+    @Override
+    public void eliminar() {
+        tab_tabla.eliminar();
+    }
+
+    public Tabla getTab_tabla() {
+        return tab_tabla;
+    }
+
+    public void setTab_tabla(Tabla tab_tabla) {
+        this.tab_tabla = tab_tabla;
+    }
+    
+}
