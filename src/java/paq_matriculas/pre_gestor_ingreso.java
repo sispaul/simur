7ed9287cbd4c79b706304c76543569ee4b9322a3
santy22.atl@gmail.com <santy22.atl@gmail.com@79e0af45-16aa-8f21-0652-82745544d00a>
2014-03-12 15:11:06 +0000

    Coreccion Requerimientos

---
+++ src/java/paq_matriculas/pre_gestor_ingreso.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paq_matriculas;
+
+import framework.componentes.Tabla;
+import paq_sistema.aplicacion.Pantalla;
+
+/**
+ *
+ * @author p-sistemas
+ */
+public class pre_gestor_ingreso extends Pantalla{
+/***DECLARACION DE TABLAS***/
+    private Tabla tab_gestor = new Tabla();
+    private Tabla tab_empresa = new Tabla();
+    
+    public pre_gestor_ingreso() {
+    }
+
+    @Override
+    public void insertar() {
+    }
+
+    @Override
+    public void guardar() {
+    }
+
+    @Override
+    public void eliminar() {
+    }
+    
+}
---
+++ src/java/paq_matriculas/pre_tipo_placa.java
@@ -0,0 +1,57 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paq_matriculas;
+
+import framework.componentes.Division;
+import framework.componentes.PanelTabla;
+import framework.componentes.Tabla;
+import paq_sistema.aplicacion.Pantalla;
+
+/**
+ *
+ * @author p-sistemas
+ */
+public class pre_tipo_placa extends Pantalla{
+
+    private Tabla tab_tabla = new Tabla();
+    
+    public pre_tipo_placa() {
+        tab_tabla.setId("tab_tabla");
+        tab_tabla.setTabla("trans_tipo_placa", "ide_tipo_placa",1);
+        tab_tabla.setHeader("TIPOS DE PLACA");
+        tab_tabla.dibujar();
+        PanelTabla pat_panel = new PanelTabla();
+        pat_panel.setPanelTabla(tab_tabla);
+
+        Division div_division = new Division();
+        div_division.setId("div_division");
+        div_division.dividir1(pat_panel);
+        agregarComponente(div_division);
+    }
+
+    @Override
+    public void insertar() {
+        tab_tabla.insertar();
+    }
+
+    @Override
+    public void guardar() {
+        tab_tabla.guardar();
+    }
+
+    @Override
+    public void eliminar() {
+        tab_tabla.eliminar();
+    }
+
+    public Tabla getTab_tabla() {
+        return tab_tabla;
+    }
+
+    public void setTab_tabla(Tabla tab_tabla) {
+        this.tab_tabla = tab_tabla;
+    }
+    
+}
